c337c373d1de1fbc2e208382b1aa06f2947f1af4
Muqeetat <aderintotolulope@gmail.com>
2026-02-23 12:50:21 +0100

    Add Scalar Documentation

--- SimpleTodoAPI/Program.cs
+++ SimpleTodoAPI/Program.cs
@@ -1,23 +1,36 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleTodoAPI.Data;
 using SimpleTodoAPI.Services;
-
+using Scalar.AspNetCore; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register the Database
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add support for Controllers
+// This generates the OpenAPI document (the JSON)
+builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
 
-// Registering the service:
-// "Scoped" means a new instance is created for every single HTTP request.
 builder.Services.AddScoped<ITodoService, TodoService>();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // This exposes the JSON file at /openapi/v1.json
+    app.MapOpenApi();
+
+    // 2. This creates the visual UI at /scalar/v1
+    app.MapScalarApiReference();
+}
+
 app.UseHttpsRedirection();
-app.MapControllers(); // This replaces all your MapGet/MapPost lines!
+app.MapControllers();
+
+app.MapGet("/", () => "SimpleTodoAPI is running");
 
 app.Run();
